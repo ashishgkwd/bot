5c5936836845464e76ddce0a7c2e338779f275cc
ashishgaikwad <ashish.gd@gmail.com>
2018-05-28 00:46:16 +0530

    creating and wiring a new component

---
+++ src/app/today/today.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css']
+})
+export class TodayComponent implements OnInit {
+
+  movieList = [
+    'Avengers: Infinity Wars',
+    'Deadpool 2',
+    'Book Club'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
