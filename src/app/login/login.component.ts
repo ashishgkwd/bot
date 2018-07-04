29d65f2da4d8ced47d8c49a277816f652a2069f9
ashishgkwd <ashish.gd@gmail.com>
2018-07-04 17:08:37 +0530

    form validation example

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  AbstractControl
+} from "@angular/forms";
 import { User } from "../models";
+import { of, Observable } from "rxjs";
+import { delay } from "rxjs/operators";
 
 @Component({
   selector: "app-login",
@@ -8,7 +15,6 @@ import { User } from "../models";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  
   loginForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
@@ -18,26 +24,58 @@ export class LoginComponent implements OnInit {
   setupLoginForm() {
     //! descriptive way to create a Form using custom fields
     this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      email: ['', Validators.required],
-      password: ['', Validators.required]
-    })
+      username: ["", [Validators.required, this.validateAdminUsername()]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required]
+    });
 
     //! another way to create a form matching any existing data model, but requires extra effort to support validators etc.
     // this.loginForm = this.formBuilder.group(new User());
   }
 
-  resetLoginForm(){
+  get email(): AbstractControl {
+    return this.loginForm.get("email");
+  }
+
+  get username(): AbstractControl {
+    return this.loginForm.get("username");
+  }
+
+  resetLoginForm() {
     this.loginForm.reset(new User());
   }
 
-  onSubmit(){
+  validateAdminUsername() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      let result = null;
+      if (control.value.toLowerCase().includes("admin")) {
+        result = { admin: control.value };
+      }
+      return result;
+    };
+  }
+
+  /* validateAdminUsernameOnServer() {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      let result = null;
+      if (control.value.toLowerCase().includes("admin")) {
+        result = { admin: control.value };
+      }
+
+      return new Promise((resolve, reject) => {
+        resolve(result), reject(null);
+      });
+      
+      //! delayed observable
+      //return of(result).pipe(delay(2000));
+    };
+  } */
+
+  onSubmit() {
     let loggedInUser = this.loginForm.value as User;
     console.log(`USER: `, loggedInUser);
     this.resetLoginForm();
   }
-
-
 
   ngOnInit() {}
 }
