336e81484db9e33c8118aefb04aa9c6b56e04368
ashishgkwd <ashish.gd@gmail.com>
2018-07-19 01:39:01 +0530

    services & DI exercise solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodayComponent } from './today/today.component';
 import { MovieListComponent } from './common/movie-list/movie-list.component';
+import { AdminComponent } from './admin/admin.component';
 import { OnlyIfDirective } from './common/directives/only-if.directive';
 import { FloatUpDirective } from './common/directives/float-up.directive';
 import { FilterPipe } from './common/pipes/filter.pipe';
 import { UpcomingBannerComponent } from './common/upcoming-banner/upcoming-banner.component';
 import { LoginComponent } from './login/login.component';
-import { AdminModule } from './admin/admin.module';
+import { SignupComponent } from './signup/signup.component';
+import { CommentsComponent } from './common/comments/comments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodayComponent,
     MovieListComponent,
+    AdminComponent,
     OnlyIfDirective,
     FloatUpDirective,
     FilterPipe,
     UpcomingBannerComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ReactiveFormsModule,
-    AdminModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  FormGroup,
-  FormBuilder,
-  Validators,
-  AbstractControl
-} from "@angular/forms";
+import { FormGroup, FormBuilder, Validators, NgForm } from "@angular/forms";
 import { User } from "../models";
-import { of, Observable } from "rxjs";
-import { delay } from "rxjs/operators";
 import { UserService } from "../services/user.service";
 
 @Component({
@@ -16,67 +9,15 @@ import { UserService } from "../services/user.service";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private userService:UserService) {
-    this.setupLoginForm();
-  }
-
-  setupLoginForm() {
-    //! descriptive way to create a Form using custom fields
-    this.loginForm = this.formBuilder.group({
-      username: ["", [Validators.required, this.validateAdminUsername()]],
-      email: ["", [Validators.required, Validators.email]],
-      password: ["", Validators.required]
-    });
-
-    //! another way to create a form matching any existing data model, but requires extra effort to support validators etc.
-    // this.loginForm = this.formBuilder.group(new User());
-  }
-
-  get email(): AbstractControl {
-    return this.loginForm.get("email");
-  }
-
-  get username(): AbstractControl {
-    return this.loginForm.get("username");
-  }
-
-  resetLoginForm() {
-    this.loginForm.reset(new User());
-  }
-
-  validateAdminUsername() {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-      let result = null;
-      if (control.value.toLowerCase().includes("admin")) {
-        result = { admin: control.value };
-      }
-      return result;
-    };
-  }
-
-  /* validateAdminUsernameOnServer() {
-    return (control: AbstractControl): { [key: string]: any } | null => {
-      let result = null;
-      if (control.value.toLowerCase().includes("admin")) {
-        result = { admin: control.value };
-      }
-
-      return new Promise((resolve, reject) => {
-        resolve(result), reject(null);
-      });
-      
-      //! delayed observable
-      //return of(result).pipe(delay(2000));
-    };
-  } */
-
-  onSubmit() {
-    let loggedInUser = this.loginForm.value as User;
-    this.userService.setLoggedInUser(loggedInUser);
-    this.resetLoginForm();
-  }
+  constructor(private userService:UserService) {}
 
   ngOnInit() {}
+
+  onSubmit(loginForm:NgForm){
+    let user = {...loginForm.value} as User;
+    this.userService.setLoggedInUser(user);
+    loginForm.reset();
+    console.log(`LOGGED-IN USER: `, user);
+  }
 }
