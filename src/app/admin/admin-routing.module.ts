a21dc3417919dbad52f5d5de817f988d80714542
ashishgkwd <ashish.gd@gmail.com>
2018-07-20 19:01:27 +0530

    nested router-outlet example

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { AdminComponent } from './admin.component';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes:Routes = [
-  {path: 'admin', component: AdminComponent}
+  {
+    path: 'admin',
+    component: AdminHomeComponent,
+    children:[
+      {path: 'new', component: AdminComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: '', redirectTo: 'new', pathMatch: 'full'}
+    ]
+  }
 ]
 
 @NgModule({
   imports: [
     RouterModule.forChild(routes),
-  ]
+  ],
+  exports:[RouterModule]
 })
 export class AdminRoutingModule { }
