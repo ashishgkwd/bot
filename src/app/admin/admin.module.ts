b553953eaf9ce51d99a58950601a4dd1b572dbf5
ashishgkwd <ashish.gd@gmail.com>
2018-07-20 19:06:12 +0530

    sync from previous branch

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminHomeComponent } from './admin-home/admin-home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   imports: [
@@ -11,7 +13,9 @@ import { AdminRoutingModule } from './admin-routing.module';
     AdminRoutingModule
   ],
   declarations: [
-    AdminComponent
+    AdminComponent,
+    AdminHomeComponent,
+    DashboardComponent
   ],
   exports: [AdminComponent]
 })
